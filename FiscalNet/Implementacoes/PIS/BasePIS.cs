dfe77079338394afa99775843cd0f28fc16b3137
Marco Polo Viana <marcopoloviana@hotmail.com>
2022-06-11 21:37:38 -0300

    Acresecentado o valor de icms nas classes de base pis e cofins  para dedução

--- FiscalNet/Implementacoes/PIS/BasePIS.cs
+++ FiscalNet/Implementacoes/PIS/BasePIS.cs
@@ -11,18 +11,21 @@ namespace FiscalNet.Implementacoes.PIS
         private decimal ValorSeguro { get; set; }
         private decimal DespesasAcessorias { get; set; }
         private decimal ValorDesconto { get; set; }
+        private decimal ValorIcms { get; set;}
 
         public BasePIS(decimal valorProduto,
             decimal valorFrete,
             decimal valorSeguro,
             decimal despesasAcessorias,
-            decimal valorDesconto)
+            decimal valorDesconto,
+            decimal valorIcms = 0)
         {
             this.ValorProduto = valorProduto;
             this.ValorFrete = valorFrete;
             this.ValorSeguro = valorSeguro;
             this.DespesasAcessorias = despesasAcessorias;
             this.ValorDesconto = valorDesconto;
+            this.ValorIcms = valorIcms;
         }
 
         public decimal CalcularBasePIS()
@@ -32,6 +35,8 @@ namespace FiscalNet.Implementacoes.PIS
                 ValorSeguro +
                 DespesasAcessorias -
                 ValorDesconto);
+
+            basePIS = basePIS - ValorIcms;
             return decimal.Round(basePIS, 2, MidpointRounding.ToEven);
         }
 
